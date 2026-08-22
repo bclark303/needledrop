5c968f5614e31a3c96f7cc7007866d8ef0b2fa23
Ben Clark <bclark303@gmail.com>
2026-08-21 22:30:52 -0400

    Add Discogs release metadata types

--- components/types.ts
+++ components/types.ts
@@ -1,4 +1,95 @@
-export type Album = { id:string; name:string; artist:string; artistId?:string; coverArt?:string; year?:number; genre?:string; songCount?:number; duration?:number; created?:string; starred?:string };
-export type Song = { id:string; title:string; artist:string; album:string; track?:number; discNumber?:number; duration?:number; coverArt?:string; suffix?:string; bitRate?:number; bitDepth?:number; samplingRate?:number };
-export type AlbumDetail = Album & { song:Song[] };
-export type VinylMeta = { pressingId?:string;pressingLabel?:string;catalogNumber?:string;country?:string;releaseYear?:number;vinylColor?:string;condition?:string;acquiredAt?:string;notes?:string;crate?:string;sideBreakAfterTrack?:number };
+export type Album = {
+  id: string;
+  name: string;
+  artist: string;
+  artistId?: string;
+  coverArt?: string;
+  year?: number;
+  genre?: string;
+  songCount?: number;
+  duration?: number;
+  created?: string;
+  starred?: string;
+};
+
+export type Song = {
+  id: string;
+  title: string;
+  artist: string;
+  album: string;
+  track?: number;
+  discNumber?: number;
+  duration?: number;
+  coverArt?: string;
+  suffix?: string;
+  bitRate?: number;
+  bitDepth?: number;
+  samplingRate?: number;
+};
+
+export type AlbumDetail = Album & { song: Song[] };
+
+export type DiscogsTrack = {
+  position: string;
+  title: string;
+  duration?: string;
+  type?: string;
+  navidromeSongId?: string;
+  navidromeIndex?: number;
+};
+
+export type DiscogsSide = {
+  label: string;
+  tracks: DiscogsTrack[];
+};
+
+export type DiscogsImage = {
+  type?: string;
+  uri?: string;
+  uri150?: string;
+  width?: number;
+  height?: number;
+};
+
+export type DiscogsCredit = {
+  name: string;
+  role?: string;
+  tracks?: string;
+};
+
+export type DiscogsIdentifier = {
+  type?: string;
+  value?: string;
+  description?: string;
+};
+
+export type VinylMeta = {
+  pressingId?: string;
+  source?: 'discogs' | 'musicbrainz' | 'manual';
+  pressingLabel?: string;
+  pressingTitle?: string;
+  catalogNumber?: string;
+  country?: string;
+  releaseYear?: number;
+  released?: string;
+  formatDescription?: string;
+  formatQuantity?: number;
+  vinylColor?: string;
+  condition?: string;
+  acquiredAt?: string;
+  notes?: string;
+  crate?: string;
+  sideBreakAfterTrack?: number;
+  discogsReleaseId?: number;
+  discogsMasterId?: number;
+  discogsUri?: string;
+  genres?: string[];
+  styles?: string[];
+  releaseNotes?: string;
+  identifiers?: DiscogsIdentifier[];
+  credits?: DiscogsCredit[];
+  images?: DiscogsImage[];
+  sides?: DiscogsSide[];
+  trackMappingWarnings?: string[];
+  discogsFetchedAt?: string;
+};
