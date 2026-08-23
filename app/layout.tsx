1fe457516e079c02b47c017e462edfba0e8235ea
Ben Clark <bclark303@gmail.com>
2026-08-23 14:24:44 -0400

    Load Record Room theme styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import './v5.css';
 import './v6.css';
 import './v7.css';
 import './v7-2.css';
+import './v8.css';
 
 export const metadata: Metadata = {
   title: { default: 'NeedleDrop', template: '%s · NeedleDrop' },
