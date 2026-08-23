dfce2456366906f42ff26ea70c7e090dc6d1b980
Ben Clark <bclark303@gmail.com>
2026-08-23 15:06:52 -0400

    Refresh PWA cache for interactive Record Room

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='needledrop-v8-0';
-const CORE=['/','/needledrop-icon.png','/needledrop-icon.svg','/record-room/audiophile-room.svg','/record-room/teen-bedroom.svg','/record-room/record-store.svg'];
+const CACHE='needledrop-v8-1';
+const CORE=['/','/needledrop-icon.png','/needledrop-icon.svg','/record-room/audiophile-room.webp','/record-room/teen-bedroom-room.webp','/record-room/record-store-room.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||event.request.url.includes('/api/'))return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
