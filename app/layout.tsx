00a527d0735ab1a8295020c8133e8694e7ce7b94
Ben Clark <bclark303@gmail.com>
2026-08-24 13:37:00 -0400

    chore: drop retired room mobile stylesheet import

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,6 @@ import './v7.css';
 import './v7-2.css';
 import './v8.css';
 import './v8-1.css';
-import './v8-1-mobile.css';
 import './v8-2.css';
 import './v8-2-theme-picker.css';
 import './v8-3.css';
